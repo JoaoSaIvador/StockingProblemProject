3c53780bfb12ad6725dfca077ec815a9317c267e
João Salvador <jpvsalvador05@gmail.com>
2021-04-28 15:12:15 +0100

    Project base code

---
+++ src/stockingproblem/StockingProblemIndividual.java
@@ -0,0 +1,83 @@
+package stockingproblem;
+
+import algorithms.Algorithm;
+import algorithms.IntVectorIndividual;
+
+public class StockingProblemIndividual extends IntVectorIndividual<StockingProblem, StockingProblemIndividual> {
+    //TODO this class might require the definition of additional methods and/or attributes
+
+    public StockingProblemIndividual(StockingProblem problem, int size) {
+        super(problem, size);
+        //TODO
+
+        for (int i=0; i < size; i++) {
+            genome[i] = i;
+        }
+
+        for (int i = 0; i < genome.length; i++) {
+            int randomIndexToSwap = Algorithm.random.nextInt(genome.length);
+            int temp = genome[randomIndexToSwap];
+            genome[randomIndexToSwap] = genome[i];
+            genome[i] = temp;
+        }
+    }
+
+    public StockingProblemIndividual(StockingProblemIndividual original) {
+        super(original);
+        //TODO
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Override
+    public double computeFitness() {
+        //TODO
+        int[][] material = new int[genome.length][problem.getMaxWidth()];
+
+        //Contar os cortes verticais
+
+        //Contar os cortes  horizontais
+
+        return this.fitness;
+    }
+
+    private boolean checkValidPlacement(Item item, int[][] material, int lineIndex, int columnIndex) {
+        int[][] itemMatrix = item.getMatrix();
+        for (int i = 0; i < itemMatrix.length; i++) {
+            for (int j = 0; j < itemMatrix[i].length; j++) {
+                if (itemMatrix[i][j] != 0) {
+                    if ((lineIndex + i) >= material.length
+                            || (columnIndex + j) >= material[0].length
+                            || material[lineIndex + i][columnIndex + j] != 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("fitness: ");
+        sb.append(fitness);
+        //TODO
+        return sb.toString();
+    }
+
+    /**
+     * @param i
+     * @return 1 if this object is BETTER than i, -1 if it is WORST than I and
+     * 0, otherwise.
+     */
+    @Override
+    public int compareTo(StockingProblemIndividual i) {
+        return (this.fitness == i.getFitness()) ? 0 : (this.fitness < i.getFitness()) ? 1 : -1;
+    }
+
+    @Override
+    public StockingProblemIndividual clone() {
+        return new StockingProblemIndividual(this);
+
+    }
+}
