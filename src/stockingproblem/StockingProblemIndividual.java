86e4d4fe80ab2abadcd47819c5375fb270d938d2
alvestr <tomasalves2001@gmail.com>
2021-04-29 16:46:40 +0100

    computeFitness todo

--- src/stockingproblem/StockingProblemIndividual.java
+++ src/stockingproblem/StockingProblemIndividual.java
@@ -3,12 +3,16 @@ package stockingproblem;
 import algorithms.Algorithm;
 import algorithms.IntVectorIndividual;
 
+import java.util.Arrays;
+
 public class StockingProblemIndividual extends IntVectorIndividual<StockingProblem, StockingProblemIndividual> {
     //TODO this class might require the definition of additional methods and/or attributes
+    private int[][] material;
 
     public StockingProblemIndividual(StockingProblem problem, int size) {
         super(problem, size);
         //TODO
+        this.material = new int[problem.getMaterialHeight()][problem.getMaxWidth()];
 
         for (int i=0; i < size; i++) {
             genome[i] = i;
@@ -20,13 +24,25 @@ public class StockingProblemIndividual extends IntVectorIndividual<StockingProbl
             genome[randomIndexToSwap] = genome[i];
             genome[i] = temp;
         }
-    }
 
-    public void placePiece(Item item, int[][] material, int lineIndex, int columnIndex){
-        for (int i = 0; i < material.length; i++) {
-            for (int j = 0; j < material[i].length; j++) {
-                material[lineIndex + i][columnIndex + j] = (char) item.getId();
-                //x + y * with
+        /*for (int i : genome) {
+            Item item = problem.getItems().get(i);
+            System.out.println(item + " letra " + item.getRepresentation());
+        }*/
+
+        for (int i = 0; i < genome.length; i++) {
+            Item itemAux = problem.getItems().get(genome[i]);
+            boolean valid = false;
+            for (int column = 0; column < problem.getMaxWidth(); column++) {
+                for (int row = 0; row < problem.getMaterialHeight(); row++) {
+                    if (valid = checkValidPlacement(itemAux, row , column)){
+                        placePiece(itemAux, row, column);
+                        break;
+                    }
+                }
+                if (valid){
+                    break;
+                }
             }
         }
     }
@@ -34,13 +50,22 @@ public class StockingProblemIndividual extends IntVectorIndividual<StockingProbl
     public StockingProblemIndividual(StockingProblemIndividual original) {
         super(original);
         //TODO
+        this.material = original.material;
+
+    }
+
+    public void placePiece(Item item, int lineIndex, int columnIndex){
+        for (int i = 0; i < item.getLines(); i++) {
+            for (int j = 0; j < item.getColumns(); j++) {
+                material[lineIndex + i][columnIndex + j] = item.getRepresentation();
+            }
+        }
     }
 
     @Override
     public double computeFitness() {
         //TODO
-        int[][] material = new int[genome.length][problem.getMaxWidth()];
-
+        //usar o menor numero de colunas
         //Contar os cortes verticais
 
         //Contar os cortes  horizontais
@@ -48,7 +73,7 @@ public class StockingProblemIndividual extends IntVectorIndividual<StockingProbl
         return this.fitness;
     }
 
-    private boolean checkValidPlacement(Item item, int[][] material, int lineIndex, int columnIndex) {
+    private boolean checkValidPlacement(Item item, int lineIndex, int columnIndex) {
         int[][] itemMatrix = item.getMatrix();
         for (int i = 0; i < itemMatrix.length; i++) {
             for (int j = 0; j < itemMatrix[i].length; j++) {
@@ -67,9 +92,22 @@ public class StockingProblemIndividual extends IntVectorIndividual<StockingProbl
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("fitness: ");
+        sb.append("Fitness: ");
         sb.append(fitness);
         //TODO
+        sb.append("\n");
+        sb.append("Genome: ");
+        sb.append(Arrays.toString(genome)).append("->").append((genome.length));
+        sb.append("\n");
+        sb.append("Best Solution Matrix: \n");
+        for (int i = 0; i < problem.getMaterialHeight(); i++) {
+            sb.append("[ ");
+            for (int j = 0; j < problem.getMaxWidth(); j++) {
+                char pieceLetter = (char) material[i][j];
+                sb.append(pieceLetter == 0 ? "-" : pieceLetter).append(" ");
+            }
+            sb.append("]\n");
+        }
         return sb.toString();
     }
 
