ae1f6442f9502781257568c17722da4126b01667
João Salvador <jpvsalvador05@gmail.com>
2021-05-05 08:31:25 +0100

    Auxiliary Functions

--- src/stockingproblem/StockingProblemIndividual.java
+++ src/stockingproblem/StockingProblemIndividual.java
@@ -13,23 +13,44 @@ public class StockingProblemIndividual extends IntVectorIndividual<StockingProbl
         super(problem, size);
         //TODO
         this.material = new int[problem.getMaterialHeight()][problem.getMaxWidth()];
-
-        for (int i=0; i < size; i++) {
-            genome[i] = i;
-        }
-
-        for (int i = 0; i < genome.length; i++) {
-            int randomIndexToSwap = Algorithm.random.nextInt(genome.length);
-            int temp = genome[randomIndexToSwap];
-            genome[randomIndexToSwap] = genome[i];
-            genome[i] = temp;
-        }
+        fillGenome(size);
+        fillMatrix();
 
         /*for (int i : genome) {
             Item item = problem.getItems().get(i);
             System.out.println(item + " letra " + item.getRepresentation());
         }*/
+    }
 
+    public StockingProblemIndividual(StockingProblemIndividual original) {
+        super(original);
+        //TODO
+        this.material = original.material;
+
+    }
+
+    private void fillGenome(int size) {
+        for (int i=0; i < size; i++) {
+            this.genome[i] = i;
+        }
+
+        for (int i = 0; i < this.genome.length; i++) {
+            int randomIndexToSwap = Algorithm.random.nextInt(this.genome.length);
+            int temp = this.genome[randomIndexToSwap];
+            this.genome[randomIndexToSwap] = this.genome[i];
+            this.genome[i] = temp;
+        }
+    }
+
+    private void placePiece(Item item, int lineIndex, int columnIndex){
+        for (int i = 0; i < item.getLines(); i++) {
+            for (int j = 0; j < item.getColumns(); j++) {
+                material[lineIndex + i][columnIndex + j] = item.getRepresentation();
+            }
+        }
+    }
+
+    private void fillMatrix() {
         for (int i = 0; i < genome.length; i++) {
             Item itemAux = problem.getItems().get(genome[i]);
             boolean valid = false;
@@ -45,22 +66,6 @@ public class StockingProblemIndividual extends IntVectorIndividual<StockingProbl
                 }
             }
         }
-
-    }
-
-    public StockingProblemIndividual(StockingProblemIndividual original) {
-        super(original);
-        //TODO
-        this.material = original.material;
-
-    }
-
-    public void placePiece(Item item, int lineIndex, int columnIndex){
-        for (int i = 0; i < item.getLines(); i++) {
-            for (int j = 0; j < item.getColumns(); j++) {
-                material[lineIndex + i][columnIndex + j] = item.getRepresentation();
-            }
-        }
     }
 
     @Override
@@ -74,20 +79,17 @@ public class StockingProblemIndividual extends IntVectorIndividual<StockingProbl
                 if (i == material.length-1 || j == material[i].length-1) {
                     break;
                 }
-
                 //Contar os cortes verticais
                 if ((char)material[i][j+1] != (char) material[i][j]) {
                     cuts++;
                 }
-
                 //Contar os cortes  horizontais
                 if ((char) material[i+1][j] != (char) material[i][j]) {
                     cuts++;
                 }
             }
         }
-
-        this.fitness = cuts;
+        this.fitness = cuts + material.length;
 
         return this.fitness;
     }
